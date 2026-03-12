d896b86c31c0c9440d2ccb5e3a5551d04bf2db65
Tamer Ahmed <tamerahmed@microsoft.com>
2026-03-12 15:50:59 -0700

    fpmsyncd: fix array bounds overflow in parseEncap
    VXLAN_RMAC is defined as 3 but tb_encap array was sized [3] (indices
    0-2), causing an out-of-bounds access at tb_encap[3]. Similarly,
    parseRtAttrNested was called with max=3, writing up to index 3 past
    the array. GCC 14 (trixie) catches this with -Werror=array-bounds.
    
    Fix array size to VXLAN_RMAC+1 and max arg to VXLAN_RMAC.
    
    Signed-off-by: Tamer Ahmed <tamerahmed@microsoft.com>

--- fpmsyncd/routesync.cpp
+++ fpmsyncd/routesync.cpp
@@ -270,11 +270,11 @@ void RouteSync::parseRtAttrNested(struct rtattr **tb, int max,
  */
 void RouteSync::parseEncap(struct rtattr *tb, uint32_t &encap_value, string &rmac)
 {
-    struct rtattr *tb_encap[3] = {0};
+    struct rtattr *tb_encap[VXLAN_RMAC + 1] = {0};
     char mac_buf[MAX_ADDR_SIZE+1];
     char mac_val[MAX_ADDR_SIZE+1];
 
-    parseRtAttrNested(tb_encap, 3, tb);
+    parseRtAttrNested(tb_encap, VXLAN_RMAC, tb);
     encap_value = ntohll((*(uint64_t *)RTA_DATA(tb_encap[LWTUNNEL_IP_ID]))) & 0xFFFFFFFF;
     memcpy(&mac_buf, RTA_DATA(tb_encap[VXLAN_RMAC]), MAX_ADDR_SIZE);
 
